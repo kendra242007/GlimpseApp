c8dc7e67fd96774ae55c11197537289048a33feb
kendra24092007 <marroquin.l.kendra.loyola@gmail.com>
2025-05-30 10:24:42 -0600

    Se agrego un nuevo formulario llamado "Tendencias"

---
+++ Tendencias.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace GlimpseApp
+{
+    partial class Tendencias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Tendencias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1604, 881);
+            this.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Tendencias";
+            this.Text = "Tendencias";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
